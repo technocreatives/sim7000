9eb15308f2dab1bbe443fc38303b79b71ec3457c
Zoey Riordan <zoey@dos.cafe>
2022-04-22 12:51:58 +0200

    Start attempting code

--- async/src/modem.rs
+++ async/src/modem.rs
@@ -1,6 +1,7 @@
 use embassy::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Sender};
 use embedded_hal::digital::blocking::OutputPin;
 use heapless::{Vec, String};
+use core::fmt::Write as FmtWrite;
 
 use crate::{read::{Read, ModemReader}, ModemContext, Error, ModemPower, PowerState, write::Write};
 
@@ -16,7 +17,7 @@ impl<'c, P: ModemPower, W: Write> Modem<'c, P, W> {
         tx: W,
         power: P,
         context: &'c ModemContext,
-    ) -> (Modem<'c, P, W>, RxPump<'c, R>) {
+    ) -> Result<(Modem<'c, P, W>, RxPump<'c, R>), Error<W::Error>> {
         let mut modem = Modem { context, power, tx };
 
         let pump = RxPump {
@@ -24,10 +25,29 @@ impl<'c, P: ModemPower, W: Write> Modem<'c, P, W> {
             generic_response: context.generic_response.sender(),
             tcp_1_channel: context.tcp_1_channel.sender(),
         };
-        
-        modem.power.disable().await;
 
-        (modem, pump)
+        modem.init().await?;
+
+        Ok((modem, pump))
+    }
+
+    async fn init(&mut self) -> Result<(), Error<W::Error>> {
+        self.power.disable().await;
+        self.power.enable().await;
+        self.run_raw_command("AT+CSCLK=0\r").await?;
+        self.run_raw_command("AT+IPR=115200\r").await?;
+        self.run_raw_command("AT+IFC=2,2\r").await?;
+        self.run_raw_command("AT+CNMP=38\r").await?;
+        self.run_raw_command("AT+CMNB=1\r").await?;
+        self.run_raw_command("AT+CFGRI=1\r").await?;
+        self.run_raw_command("AT+CEDRXS=1,4,1011\r").await?;
+        self.power.disable().await;
+        Ok(())
+    }
+
+    pub async fn activate(&mut self) {
+        self.power.enable().await;
+
     }
 
     pub async fn run_raw_command(&mut self, command: &str) -> Result<Vec<String<32>, 4>, Error<W::Error>> {
@@ -46,6 +66,14 @@ impl<'c, P: ModemPower, W: Write> Modem<'c, P, W> {
     }
 }
 
+enum ModemState {
+    Disabled,
+    Sleeping,
+    Errored,
+    Initializing,
+    Running
+}
+
 pub struct RxPump<'context, R> {
     reader: ModemReader<R>,
     generic_response: Sender<'context, CriticalSectionRawMutex, heapless::String<256>, 1>,
@@ -59,4 +87,9 @@ impl<'context, R: Read> RxPump<'context, R> {
         self.generic_response.send(line).await;
         Ok(())
     }
+}
+
+pub struct TxPump<'context, W> {
+    writer: W,
+    messages: Receiver<'context, CriticalSectionRawMutex, heapless::String<256>, 8>,
 }
