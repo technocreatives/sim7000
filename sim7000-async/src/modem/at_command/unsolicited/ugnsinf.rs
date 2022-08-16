50a06c11da1873b39434569164c45791f89a9c96
Joakim Hulthe <joakim@hulthe.net>
2022-08-16 17:13:18 +0200

    Fix ugnsinf parsing

--- sim7000-async/src/modem/at_command/unsolicited/ugnsinf.rs
+++ sim7000-async/src/modem/at_command/unsolicited/ugnsinf.rs
@@ -16,12 +16,15 @@ pub enum GnssReport {
         vdop: f32,
         speed_over_ground: f32,
         course_over_ground: f32,
-        satellites_in_view: u32,
-        satellites_used: u32,
+        sat_gps_in_view: u32,
+        sat_gnss_used: u32,
+        sat_glonass_used: u32,
         signal_noise_ratio: u32,
     },
 }
 
+// TODO: unit tests. example:
+// UGNSINF: 1,1,20220815113233.000,57.715366,11.973866,120.500,0.00,0.0,1,,0.9,1.1,0.7,,20,8,5,,49,,
 impl ATParseLine for GnssReport {
     fn from_line(line: &str) -> Result<Self, ATParseErr> {
         let (message, rest) = line.split_once(": ").ok_or(ATParseErr)?;
@@ -30,22 +33,7 @@ impl ATParseLine for GnssReport {
             return Err(ATParseErr);
         }
 
-        // NOTE: +UGNSINF differs *slightlly from +CGNSINF
-        //
-        // +UGNSINF: <GNSS run status>,<Fix status>,<UTC date & Time>,
-        // <Latitude>,<Longitude>,<MSL Altitude>,<Speed Over Ground>,
-        // <Course Over Ground>,<Fix Mode>,<Reserved1>,<HDOP>,
-        // <PDOP>,<VDOP>,<Reserved2>,<Satellites in View>,
-        // <Satellites Used>,<Reserved3>,<C/N0 max>,<HPA>,<VPA>
-        //
-        //
-        // +CGNSINF: <GNSS run status>,<Fix status>,<UTC date & Time>,
-        // <Latitude>,<Longitude>,<MSL Altitude>,<Speed Over Ground>,
-        // <Course Over Ground>,<Fix Mode>,<Reserved1>,<HDOP>,
-        // <PDOP>,<VDOP>,<Reserved2>,<GNSS Satellites in View>,
-        // <GNSS Satellites Used>,<GLONASS Satellites Used>,<Reserved3>,
-        // <C/N0 max>,<HPA>,<VPA>
-        let [run_status, fix_status, _utc_datetime, latitude, longitude, msl_altitude, speed_over_groud, course_over_ground, _fix_mode, _reserved1, hdop, pdop, vdop, _reserved2, satellites_in_view, satellites_used, _reserved3, c_n0_max, _hpa, _vpa] =
+        let [run_status, fix_status, _utc_datetime, latitude, longitude, msl_altitude, speed_over_groud, course_over_ground, _fix_mode, _reserved1, hdop, pdop, vdop, _reserved2, sat_gps_in_view, sat_gnss_used, sat_glonass_used, _reserved3, c_n0_max, _hpa, _vpa] =
             collect_array(rest.split(',')).ok_or(ATParseErr)?;
 
         if run_status != "1" {
@@ -54,7 +42,7 @@ impl ATParseLine for GnssReport {
 
         if fix_status != "1" {
             return Ok(GnssReport::NoFix {
-                sat_gps_view: satellites_in_view.parse().ok(),
+                sat_gps_view: sat_gps_in_view.parse().ok(),
             });
         }
 
@@ -67,9 +55,13 @@ impl ATParseLine for GnssReport {
             hdop: hdop.parse()?,
             pdop: pdop.parse()?,
             vdop: vdop.parse()?,
-            satellites_in_view: satellites_in_view.parse()?,
-            satellites_used: satellites_used.parse()?,
             signal_noise_ratio: c_n0_max.parse()?,
+
+            // The docs contradicts itself on what these values are and what they are called
+            // TODO: Make sure these are correct.
+            sat_gps_in_view: sat_gps_in_view.parse()?,
+            sat_gnss_used: sat_gnss_used.parse()?,
+            sat_glonass_used: sat_glonass_used.parse()?,
         })
     }
 }
