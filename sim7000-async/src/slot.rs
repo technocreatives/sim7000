4b28233ecdbdba6708f40d18ba25a3291bbdb71e
Joakim Hulthe <joakim@hulthe.net>
2022-11-24 15:34:18 +0100

    Make Slot default value be zeroable

--- sim7000-async/src/slot.rs
+++ sim7000-async/src/slot.rs
@@ -1,10 +1,11 @@
 use core::any::type_name;
+use core::ops::Not;
 use core::sync::atomic::{AtomicBool, Ordering};
 
 use crate::log;
 
 pub(crate) struct Slot<T: 'static> {
-    is_free: AtomicBool,
+    is_claimed: AtomicBool,
     inner: T,
 }
 
@@ -12,14 +13,15 @@ impl<T: 'static> Slot<T> {
     pub const fn new(inner: T) -> Self {
         Slot {
             inner,
-            is_free: AtomicBool::new(true),
+            is_claimed: AtomicBool::new(false),
         }
     }
 
     /// Try to claim the slot, returns None if the slot has already been claimed
     pub fn claim(&self) -> Option<&T> {
-        self.is_free
-            .fetch_and(false, Ordering::Relaxed)
+        self.is_claimed
+            .fetch_or(true, Ordering::Relaxed)
+            .not()
             .then(|| &self.inner)
     }
 
@@ -30,12 +32,33 @@ impl<T: 'static> Slot<T> {
 
     /// Release the claim on the slot
     pub fn release(&self) {
-        if self.is_free.fetch_or(true, Ordering::Relaxed) {
+        if !self.is_claimed.fetch_and(false, Ordering::Relaxed) {
             log::error!("Tried to release unclaimed Slot<{:?}>", type_name::<T>());
         }
     }
 
     pub fn is_free(&self) -> bool {
-        self.is_free.fetch_or(false, Ordering::Relaxed)
+        !self.is_claimed.fetch_or(false, Ordering::Relaxed)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Slot;
+
+    #[test]
+    fn slot() {
+        let slot = Slot::new(());
+
+        for _ in 0..3 {
+            assert!(slot.is_free());
+            assert!(slot.is_free());
+            assert!(slot.claim().is_some());
+            assert!(!slot.is_free());
+            assert!(!slot.is_free());
+            assert!(slot.claim().is_none());
+            assert!(!slot.is_free());
+            slot.release();
+        }
     }
 }
