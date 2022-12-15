94335c66485dfc6e71184c050157d9c45fc0ac14
Joakim Hulthe <joakim@hulthe.net>
2022-12-15 17:32:48 +0100

    Document drop.rs more

--- sim7000-async/src/drop.rs
+++ sim7000-async/src/drop.rs
@@ -1,3 +1,23 @@
+//! Types for facilitating asynchronous dropping.
+//!
+//! For certain types it is necessary to run the descructor asynchronously. This is impossible with
+//! the regular Drop trait.
+//!
+//! This module provides the [AsyncDrop] type, which takes a [DropChannel] and a [DropMessage], and
+//! all it does is pass the [DropMessage] to the channel when the [AsyncDrop] is dropped. The
+//! intended use-case is embedding the [AsyncDrop] in another struct, and then letting the drop
+//! logic happen implicitly.
+//!
+//! The actual drop-logic will take place on another task dedicated to polling the [DropChannel]
+//! (see [DropPump](crate::pump::DropPump)).
+//!
+//! Example
+//! ```
+//! struct TypeWithAnAsyncDescructor {
+//!    _drop: AsyncDrop<'static>,
+//! }
+//! ```
+
 use core::mem::drop;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
 
@@ -8,11 +28,12 @@ use crate::modem::{CommandRunnerGuard, ModemContext};
 use crate::tcp::CONNECTION_SLOTS;
 use crate::Error;
 
-/// The capacity of the drop channel
+/// The capacity of the drop channel.
 /// Nust be at least the number of unique objects that can be dropped.
 const DROP_CAPACITY: usize = GNSS_SLOTS + CONNECTION_SLOTS;
 pub type DropChannel = Channel<CriticalSectionRawMutex, DropMessage, DROP_CAPACITY>;
 
+/// Type for facilitating asynchronous dropping. See module-level docs for details.
 pub struct AsyncDrop<'c> {
     channel: &'c DropChannel,
     message: DropMessage,
@@ -32,15 +53,21 @@ impl Drop for AsyncDrop<'_> {
     }
 }
 
-/// This type facilitates asynchronous dropping.
+/// This type facilitates asynchronous dropping. See module-level docs for details.
+///
+/// This enum has one variant for each kind of type which needs asynchronous dropping.
 #[derive(Clone, Copy, Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum DropMessage {
+    /// Drop a [TcpStream](crate::tcp::TcpStream).
     Connection(usize),
+
+    /// Drop a [Gnss](crate::gnss::Gnss).
     Gnss,
 }
 
 impl DropMessage {
+    /// Run the Drop logic for this message.
     pub async fn run(&self, runner: &mut CommandRunnerGuard<'_>) -> Result<(), Error> {
         log::debug!("Sending drop command for {:?}", self);
 
@@ -57,15 +84,21 @@ impl DropMessage {
                 runner
                     .run(CloseConnection { connection: n })
                     .await
-                    .map(drop)
                     // Closing is allowed to fail, since this might happen if the connection was
                     // closed on the remote before we closed it here.
-                    .or_else(sim_may_fail)
+                    .map(drop)
+                    .or_else(sim_may_fail)?;
             }
-            DropMessage::Gnss => runner.run(SetGnssPower(false)).await.map(drop),
+            DropMessage::Gnss => {
+                runner.run(SetGnssPower(false)).await?;
+            }
         }
+
+        Ok(())
     }
 
+    /// Clean up logic that gets run after running the drop logic, regardless of whether the drop
+    /// logic errored or not.
     pub fn clean_up(&self, ctx: &ModemContext) {
         log::debug!("Cleaning up after {:?}", self);
         match self {
