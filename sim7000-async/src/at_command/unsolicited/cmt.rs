7019f589aebf2396fdc3ae071c8b6c08d6fd5070
Joakim Hulthe <joakim@hulthe.net>
2022-12-29 15:38:03 +0100

    Fix line endings & clean up unused files

--- sim7000-async/src/at_command/unsolicited/cmt.rs
+++ sim7000-async/src/at_command/unsolicited/cmt.rs
@@ -1,11 +1,12 @@
-use crate::modem::at_command::{stub_parser_prefix, ATParseErr, ATParseLine};
+use crate::at_command::{stub_parser_prefix, AtParseErr, AtParseLine};
 
 // stub type
 #[derive(Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct Cmt;
 
-impl ATParseLine for Cmt {
-    fn from_line(line: &str) -> Result<Self, ATParseErr> {
+impl AtParseLine for Cmt {
+    fn from_line(line: &str) -> Result<Self, AtParseErr> {
         stub_parser_prefix(line, "+CMT:", Cmt)
     }
 }
--- sim7000-async/src/at_command/unsolicited/cmti.rs
+++ sim7000-async/src/at_command/unsolicited/cmti.rs
@@ -1,11 +1,12 @@
-use crate::modem::at_command::{stub_parser_prefix, ATParseErr, ATParseLine};
+use crate::at_command::{stub_parser_prefix, AtParseErr, AtParseLine};
 
 // stub type
 #[derive(Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct Cmti;
 
-impl ATParseLine for Cmti {
-    fn parse_from_line(line: &str) -> Result<Self, ATParseErr> {
+impl AtParseLine for Cmti {
+    fn from_line(line: &str) -> Result<Self, AtParseErr> {
         stub_parser_prefix(line, "+CMTI:", Cmti)
     }
 }
